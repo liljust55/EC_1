593958efee6093afc0f1b28eb031f3306e58680e
liljust55 <40484288+liljust55@users.noreply.github.com>
2024-04-15 05:15:07 +0800

    init files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Shop } from "./Pages/Shop";
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes/>
+        <Route path='/' element={<Shop/>} />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
